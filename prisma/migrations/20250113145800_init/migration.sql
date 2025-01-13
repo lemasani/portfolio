ea07698dd47af4b575bb5321dc96981f32d52807
lemasani <brianlemasan@gmail.com>
2025-01-13 18:01:24 +0300

    updated driver

--- prisma/migrations/20250113145800_init/migration.sql
+++ prisma/migrations/20250113145800_init/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NULL,
+    `password` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -15,8 +16,8 @@ CREATE TABLE `Project` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NOT NULL,
+    `githubUrl` VARCHAR(191) NOT NULL,
     `imageUrl` VARCHAR(191) NULL,
-    `githubUrl` VARCHAR(191) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
